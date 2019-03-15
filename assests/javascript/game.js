9411fb5e7c35ab608890ad29715a7901766b7cb7
Ben Leone <benleone90@users.noreply.github.com>
2019-03-14 22:05:47 -0400

    updated js and html files

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,5 +1,4 @@
-// Create variable array of words to choose from
-// and variables for wins, losses, and tries
+// Create variables for the game
 var words = [
     "lightsaber",
     "jedi",
@@ -9,28 +8,63 @@ var words = [
     "skywalker",
     "vader"
 ];
-var answerArray = [];
+var guessWord = [];
 var won = 0;
 var loss = 0;
-var attempts = 10;
-var usedArray = [];
+var maxAttempts = 10;
+var guessLetters = [];
+var gameStart = false;
+var gameFinish = false;
+
+// Reset game variables for new game
+function resetGame() {
+    maxAttempts = 10;
+    gameStart = false;
+
+    // Get random word from the words array
+    currentWord = Math.floor(Math.random() * words.length);
+
+    // Clear Word and Letters arrays
+    guessLetters = [];
+    guessWord = [];
+
+    // Push underscores to selected word
+    for (var i = 0; i < words[currentWord].length; i++) {
+        guessWord.push("_")
+    }
+
+    // Update Screen
+    updateScreen();
+}
+
+// Updates data on HTML Page
+function updateScreen() {
+    document.getElementById("wins").innerHTML = won;
+    document.getElementById("wordGuess").innerHTML = "";
+    for (var j = 0, j < guessWord.length; i++) {
+        document.getElementById("wordGuess").innerHTML += guessWord[i];
+    }
+}
+
+
+
 
 //Choosing word from array and displaying to console
-var pickWord =  words[Math.floor(Math.random()*words.length)];
-console.log(pickWord);
+// var pickWord =  words[Math.floor(Math.random()*words.length)];
+// console.log(pickWord);
 
-document.getElementById("wins").innerHTML = "Your Wins: " + won;
-document.getElementById("losses").innerHTML = "Your Losses: " + loss;
-document.getElementById("guessLeft").innerHTML = "Remaining Guesses: " + attempts;
-document.getElementById("pastGuess").innerHTML = "Your Guesses: " + usedArray;
-document.getElementById("wordGuess").innerHTML = "Guess the Word: " + answerArray;
+// document.getElementById("wins").innerHTML = "Your Wins: " + won;
+// document.getElementById("losses").innerHTML = "Your Losses: " + loss;
+// document.getElementById("guessLeft").innerHTML = "Remaining Guesses: " + attempts;
+// document.getElementById("pastGuess").innerHTML = "Your Guesses: " + usedArray;
+// document.getElementById("wordGuess").innerHTML = "Guess the Word: " + answerArray;
 
-//Function to fill pickWord length with underscores//
-var answerArraySetup = function(_pickWord){
-    for (var i=0; i<pickWord.length; i++){
-        answerArray.push("_");
-    }
-    return answerArray
-}
+// //Function to fill pickWord length with underscores//
+// var answerArraySetup = function(_pickWord){
+//     for (var i=0; i<pickWord.length; i++){
+//         answerArray.push("_");
+//     }
+//     return answerArray
+// }
 
  
