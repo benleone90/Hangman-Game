bd9f36a9350a28261479f6b2674940fd34ce2d66
Ben Leone <benleone90@users.noreply.github.com>
2019-04-11 08:58:57 -0400

    updating JS logic

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -24,3 +24,56 @@ var wrongGuesses = [];
 var winCounter = 0;
 var lossCounter = 0;
 var numGuesses = 9;
+
+// startGame funtion to start and reset the game
+function startGame() {
+  // Reset guesses to 0
+  numGuesses = 9;
+
+  // One of the words from the array is randomly chosen
+  chosenWord = words[Math.floor(Math.random() * words.length)];
+  // chosenWord is then broken into the individual letters
+  lettersInWord = chosenWord.split("");
+  // Count the number of letters in the chosen word
+  numBlanks = lettersInWord.length;
+
+  // Console log the chosen word to ensure it's being read
+  console.log(chosenWord);
+
+  // Reset the arrays holding the letters and blanks and the wrong guesses
+  mixedArr = [];
+  wrongGuesses = [];
+
+  // Use a for loop to fill the mixedArr with the correct number of blanks
+  for (var i = 0; i < numBlanks; i++) {
+    mixedArr.push("_");
+  }
+  console.log(mixedArr);
+
+  // Print blanks in the html work-blanks area
+  document.getElementById("word-blanks").innerHTML = mixedArr.join(" ");
+  // Prints number of guess back to 9
+  document.getElementById("guesses-left").innerHTML = numGuesses;
+  // Clear the wrong guesses from the previous round
+  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+}
+
+//Function to check the letters in the word
+function checkLetters(letter) {
+  var letInWord = false;
+
+  // Check to see if the letter is in the word with loop
+  for (var i = 0; i < numBlanks; i++) {
+    if (chosenWord[i] === letter) {
+      letInWord = true;
+    }
+  }
+  // If the letter exsits in the word, find out exactly what index
+  if (letInWord) {
+    for (j = 0; j < numBlanks; j++) {
+      if (chosenWord[j] === letter) {
+        mixedArr[j] = letter;
+      }
+    }
+  }
+}
